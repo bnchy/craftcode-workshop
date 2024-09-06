0937af24a2ec00c74d031a1f824a2502c89a6131
jellespruyt <bnchy@proton.me>
2024-09-06 15:20:53 +0200

    feat:added frontend navigation and routing

---
+++ frontend-beer-frontier/src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatButtonModule, MatMenuModule, RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
