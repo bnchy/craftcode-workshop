e221f9ecf91c2a9d7771a92118759a0d1e1f09a6
jellespruyt <bnchy@proton.me>
2024-09-13 11:01:23 +0200

    feat: added edit blueprint for beers, TODO: backend endpoint

--- frontend-beer-frontier/src/app/pages/beers/beer/beer.component.ts
+++ frontend-beer-frontier/src/app/pages/beers/beer/beer.component.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { BeerService } from '../../../services/beer.service';
-import { Beer, Classification } from '../../../api';
+import { Beer } from '../../../api';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { transformEnumValue } from '../../../utils/string-utils';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-beer',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatDividerModule, RouterLink],
+  imports: [    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatCardModule,
+    RouterLink],
   templateUrl: './beer.component.html',
   styleUrls: ['./beer.component.scss']
 })
 export class BeerComponent implements OnInit {
+  edit: boolean = false;
   beer: Beer | undefined;
 
   constructor(private beerService: BeerService, private route: ActivatedRoute) {}
